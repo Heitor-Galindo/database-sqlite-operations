4d9f563c632bfd29f9ff05dac2b7f6c14a04589f
heitor.galindo <heitor.galindo@supersim.com.br>
2022-05-03 20:44:34 -0300

    5th commit

--- teste.py
+++ teste.py
@@ -1,13 +1,14 @@
 """ . """
-
 from database_folder.select_nfce_url import get_max, get_url
 
 def select():
     """ . """
     max_row = get_max()
     next_row = get_url()[1]
+    print(f"max row: {max_row}", type(max_row))
+    print(f"next row: {next_row}", type(next_row), "\n")
 
-    while int(next_row) <= int(max_row):
+    while int(next_row) <= max_row:
 
         print(f"max row: {max_row}", type(max_row))
         print(f"next row: {next_row}", type(next_row))
